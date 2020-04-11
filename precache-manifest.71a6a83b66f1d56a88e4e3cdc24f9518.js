862441aa89c405c7d70080ec808d51898768261d
Milosz Gawel <mgawel@gmail.com>
2020-04-11 02:26:08 +0200

    Updates

---
+++ precache-manifest.71a6a83b66f1d56a88e4e3cdc24f9518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ec4d9e580f20f32662ac2c4a750687",
+    "url": "/organiseIT/index.html"
+  },
+  {
+    "revision": "a2d5dec34c515be5fb1d",
+    "url": "/organiseIT/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a2d5dec34c515be5fb1d",
+    "url": "/organiseIT/static/js/2.aa27b23e.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/organiseIT/static/js/2.aa27b23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e8207314f88ff85b25",
+    "url": "/organiseIT/static/js/main.fa2ffc96.chunk.js"
+  },
+  {
+    "revision": "217dff4aeebd3a5fa86d",
+    "url": "/organiseIT/static/js/runtime-main.5acaec3f.js"
+  }
+]);
